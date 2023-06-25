45f195006be2b14df2d76e577542fc242716355d
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-06-24 21:05:31 -0500

    adding client details

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
 provider "tfe" {}
 
-provider "azuread" {}
+provider "azuread" {
+    client_id = "b61d8ad4-cc41-48f1-9549-7b6744696c61"
+    tenant_id = "57538465-cb2a-4830-b003-8519e7143eb8"
+    client_secret = var.client_secret
+}
